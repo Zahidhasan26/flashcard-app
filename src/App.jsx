c8ae51ac24e184a21577df248f0ea5798a1999ff
Zahid Hasan <hasanzahid74@gmail.com>
2025-03-08 01:54:59 -0500

    all basics complete

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,13 @@ import './App.css';
 
 function App() {
   const flashcards = [
-    { question: 'What is the capital of France?', answer: 'Paris' },
-    { question: 'What is 2 + 2?', answer: '4' },
-    { question: 'What is the largest planet in the solar system?', answer: 'Jupiter' },
+    { question: 'He is a Saiyan from the planet Vegeta.' +
+       'He is known for his signature move, the Kamehameha.'+
+       'He has spiky black hair that turns golden when he transforms into a Super Saiyan.', answer: 'Goku' },
+    { question: 'He dreams of becoming the Hokage of his village.'+ 'He has a Nine-Tailed Fox sealed inside him.' + 'He is known for his catchphrase, Believe it! (Dattebayo!).', answer: 'Naruto' },
+    { question: 'He is the captain of the Straw Hat Pirates. He gained the ability to stretch his body after eating the Gum-Gum Fruit. He is on a quest to find the legendary treasure known as One Piece.', answer: 'Monkey D Luffy' },
+    { question: 'He possesses the ability to transform into a Titan. He seeks revenge against the Titans for destroying his hometown. His catchphrase is "I will destroy the entire world."', answer: 'Eren Yeager' },
+    { question: 'She is a magical girl who fights evil in the name of the Moon. She wields a magical brooch and a Moon Stick. She is known for her iconic phrase, "In the name of the Moon, I will punish you!"', answer: 'SaiLOR Moon' },
   ];
 
   const [currentCardIndex, setCurrentCardIndex] = useState(0);
@@ -23,7 +27,9 @@ function App() {
 
   return (
     <div className="flashcard-container">
-      <h1>Flashcard App</h1>
+      <h1>Guess The Anime Character!</h1>
+      <p>There will be some hints and you need to guess which Anime character it is.</p>
+      <p>Total Flashcards : 5</p>
       <div
         className={`flashcard ${isFlipped ? 'flipped' : ''}`}
         onClick={handleFlip}
